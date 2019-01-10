2ba497c997c8c747f63b1f32333e70a87e49c456
Gustavo <consugus@gmail.com>
2019-01-10 20:20:36 -0300

    Lectura a host remoto

--- includes/funciones/dbconnection.php
+++ includes/funciones/dbconnection.php
@@ -1,5 +1,21 @@
 <?php
-    $conn = new mysqli('localhost', 'root', 'root', 'gldwebcamp');
+    // $host = 'localhost';
+    // $usuario_DB = 'root';
+    // $password_DB = 'root';
+    // $database = 'gldwebcamp';
+
+    $host = '159.89.159.137';// '159.89.159.137:3306';
+    $usuario_DB = 'user-staging';
+    $password_DB = 'qmRH40pQgtLMarP0';
+    $database = 'camp_consugus';
+
+
+    $conn = new mysqli($host, $usuario_DB, $password_DB, $database);
+
+    // echo '<pre>';
+    //     var_dump($conn);
+    // echo '</pre>';
+
 
     if($conn->connect_error){
         echo $error->$conn->connect_error;
